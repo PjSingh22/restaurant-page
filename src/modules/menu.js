018e6c48fab04d1ce7c981b749abec05a1c3f60d
Prabhjot Singh <pjsingh1359@gmail.com>
2022-03-24 21:19:13 -0700

    imported photos into js files

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,30 +1,36 @@
+import fancyPants from '../fancy-pants.jpg';
+import orangeTang from '../orange-tang.jpg';
+import surpriseMe from '../surprise-me.jpg';
+import sushiPlatter from '../sushi-platter.jpg';
+import sushicado from '../sushicado.jpg';
+import theBasics from '../the-basics.jpg';
 const menu = () => {
   return `
   <div class="menu-info">
     <h2 class="about page-title">Menu</h2>
     <div class="menu-items">
       <div class="item">
-        <img src="../src/fancy-pants.jpg" alt="fancy pants sushi" srcset="" class="item-img">
+        <img src="${fancyPants}" alt="fancy pants sushi" srcset="" class="item-img">
         <p class="item-name">Fancy Pants</p>
       </div>
       <div class="item">
-        <img src="../src/orange-tang.jpg" alt="orange tang" srcset="" class="item-img">
+        <img src="${orangeTang}" alt="orange tang" srcset="" class="item-img">
         <p class="item-name">Orange-Tang</p>
       </div>
       <div class="item">
-        <img src="../src/surprise-me.jpg" alt="surprise me" srcset="" class="item-img">
+        <img src="${surpriseMe}" alt="surprise me" srcset="" class="item-img">
         <p class="item-name">Surprise me</p>
       </div>
       <div class="item">
-        <img src="../src/sushi-platter.jpg" alt="sushi platter" srcset="" class="item-img">
+        <img src="${sushiPlatter}" alt="sushi platter" srcset="" class="item-img">
         <p class="item-name">The Platter</p>
       </div>
       <div class="item">
-        <img src="../src/sushicado.jpg" alt="sushicado" srcset="" class="item-img">
+        <img src="${sushicado}" alt="sushicado" srcset="" class="item-img">
         <p class="item-name">Sushicado</p>
       </div>
       <div class="item">
-        <img src="../src//the-basics.jpg" alt="Basic sushi dish" srcset="" class="item-img">
+        <img src="${theBasics}" alt="Basic sushi dish" srcset="" class="item-img">
         <p class="item-name">The Basics</p>
       </div>
     </div>
